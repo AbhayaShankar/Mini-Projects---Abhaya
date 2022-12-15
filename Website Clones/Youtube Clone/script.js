5518a872afd0d2549882f7c7de5df97e93602602
Abhaya <shankarabhaya9@gmail.com>
2022-12-16 00:22:41 +0530

    responsive started

--- Website Clones/Youtube Clone/script.js
+++ Website Clones/Youtube Clone/script.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
 
 menuIcon.onclick = () => {
   sidebar.classList.toggle("hide-sidebar");
+  container.classList.toggle("large-container");
 };
